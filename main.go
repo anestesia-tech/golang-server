a8007a7e9c04d6bf96f4240a7929b946d0abecac
anestesia <anestesiagapochkina@yandex.ru>
2024-11-04 08:29:30 +0000

    Add main code for server

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"log"
+)
+
+func hello(w, http.ResponseWriter, r *httpRequest) {
+	fmt.Fprintf(w, "Hello!")
+}
+
+func main() {
+	http.HandleFunc("/", hello)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAdnServe: ", err)
+	}
+}
+
